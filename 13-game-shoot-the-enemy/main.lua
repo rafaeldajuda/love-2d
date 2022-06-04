3a40ec6b0c38535c9842f348bc55b9c18ce7cdb1
Rafael <rafaeldajuda@gmail.com>
2022-06-04 14:44:14 -0300

    game shoot enemy

---
+++ 13-game-shoot-the-enemy/main.lua
@@ -0,0 +1,43 @@
+function love.load()
+    Object = require "libs.classic"
+    require "player"
+    require "enemy"
+    require "bullet"
+
+    player = Player()
+    enemy = Enemy()
+    listOfBullets = {}
+end
+
+function love.update(dt)
+    player:update(dt)
+    enemy:update(dt)
+
+    for i, v in ipairs(listOfBullets) do
+        v:update(dt)
+
+        -- Each bullets checks if there is collision with the enemy
+        v:checkCollision(enemy)
+
+        -- If the bullet has the property dead and it's true then..
+        if v.dead then
+            -- Remove it from the list
+            table.remove(listOfBullets, i)
+        end
+    end
+end
+
+function love.draw()
+
+    love.graphics.print("spacebar to shoot", 10, 10)
+    player:draw()
+    enemy:draw()
+
+    for i, v in ipairs(listOfBullets) do
+        v:draw()
+    end
+end
+
+function love.keypressed(key)
+    player:keyPressed(key)
+end
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # love-2d
 :)
+
+## refs:
+[love 2D wiki](https://love2d.org/wiki/Main_Page)<br/>
+[tutorial - sheepolution](https://sheepolution.com/learn/book/contents)
