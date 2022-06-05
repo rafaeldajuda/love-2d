5b4de3d38acdaa6b6c297312a2bb4eacb1bb41fd
Rafael <rafaeldajuda@gmail.com>
2022-06-05 16:46:43 -0300

    bug food resolve

--- projects/01-game/main.lua
+++ projects/01-game/main.lua
@@ -7,6 +7,7 @@ function love.load()
     windowHeight = love.graphics.getWidth()
     tileWidth = windowWidth / 5
     tileHeight = windowHeight / 5
+    newFood = true
 
     -- Map
     mapOfTiles = createMap(tileWidth, tileHeight)
@@ -21,7 +22,6 @@ function love.load()
 
     -- Actor Food positon
     mapOfTiles[1][1] = actor
-    mapOfTiles[3][3] = food
 
     time = 0
 end
@@ -32,7 +32,9 @@ function love.update(dt)
     print("time\t", time)
     if time >= 1 then
         moveActor(actor.direction)
-        time = 0
+        if newFood then
+            foodNewPosition()
+        end
     end
     
     print("actor x:\t",actor.x)
@@ -40,6 +42,8 @@ function love.update(dt)
     print("actor direction:",actor.direction)
     print("food x:\t", food.x)
     print("food y:\t", food.y)
+    print("random", math.random(0, 5))
+    print("tileWidth", tileWidth)
     print("/-------------------------/")
    
 end
@@ -47,19 +51,27 @@ end
 function love.draw()
 
     -- print mapOfTiles
-    love.graphics.setColor(1, 1, 1)
     for i, line in ipairs(mapOfTiles) do
         for j, tile in ipairs(line) do
-            love.graphics.rectangle(tile.mode, tile.x, tile.y, tile.width, tile.height)
+            if tile.type == "tile" then
+                print("tile", i, j)
+                love.graphics.setColor(1, 1, 1)
+                love.graphics.rectangle(tile.mode, tile.x, tile.y, tile.width, tile.height) 
+            elseif tile.type == "actor" then
+                -- draw actor
+                print("actor", i, j)
+                love.graphics.setColor(1, 1, 1)
+                love.graphics.rectangle(tile.mode, tile.x, tile.y, tile.width, tile.height) 
+            else
+                 -- draw food
+                print("food", i, j)
+                love.graphics.setColor(1, 0, 0)
+                love.graphics.rectangle(tile.mode, tile.x, tile.y, tile.width, tile.height) 
+            end   
+            
         end
     end
 
-    -- draw actor
-    love.graphics.rectangle(actor.mode, actor.x, actor.y, actor.width, actor.height)
-
-    -- draw food
-    love.graphics.setColor(1, 0, 0)
-    love.graphics.rectangle(food.mode, food.x, food.y, food.width, food.height)
 end
 
 -- Functions    ----------------------------------------------------------------------
@@ -71,67 +83,73 @@ end
 function moveActor(direction)
     
     if direction == "right" then
-        newActorPosition(actor, "x", 1)
+        newActorPosition("x", 1)
     elseif direction == "left" then
-        newActorPosition(actor, "x", -1)
+        newActorPosition("x", -1)
     elseif direction == "up" then
-        newActorPosition(actor, "y", -1)
+        newActorPosition("y", -1)
     elseif direction == "down" then
-        newActorPosition(actor, "y", 1)
+        newActorPosition("y", 1)
     end
 
 end
 
-function newActorPosition(obj, axis, direction)
-    local actor_x_position = (obj.x / tileWidth) + 1
-    local actor_y_position = (obj.y / tileHeight) + 1
-    local new_tile = createTileMap("line", obj.x, obj.y, tileWidth, tileHeight, "tile")
+function newActorPosition(axis, direction)
+    local actor_x_position = (actor.x / tileWidth) + 1
+    local actor_y_position = (actor.y / tileHeight) + 1
+    local new_tile = createTileMap("line", actor.x, actor.y, tileWidth, tileHeight, "tile")
+    time = 0
 
     if axis == "x" then
         local actor_map_new_position = actor_x_position + 1 * direction
-        local actor_new_position = obj.x + tileWidth * direction
+        local actor_new_position = actor.x + tileWidth * direction
         if actor_new_position < windowWidth and actor_new_position >= 0 then
-            if mapOfTiles[actor_map_new_position][actor_y_position].type == "food" then
-                foodNewPosition()
+            actor.x = actor_new_position
+            if mapOfTiles[actor_map_new_position][actor_y_position] == food then
+                print("come x")
+                newFood = true
             end
-
-            obj.x = actor_new_position
-            mapOfTiles[actor_map_new_position][actor_y_position] = obj
+            mapOfTiles[actor_map_new_position][actor_y_position] = actor
             mapOfTiles[actor_x_position][actor_y_position] = new_tile
         end
-    else
+        return
+    elseif axis == "y" then
         local actor_map_new_position = actor_y_position + 1 * direction
-        local actor_new_position = obj.y + tileHeight * direction
+        local actor_new_position = actor.y + tileHeight * direction
         if actor_new_position < windowWidth and actor_new_position >= 0  then
-            if mapOfTiles[actor_x_position][actor_map_new_position].type == "food" then
-                foodNewPosition()
+            actor.y = actor_new_position
+            if mapOfTiles[actor_x_position][actor_map_new_position] == food then
+                print("come y")
+                newFood = true
             end
-
-            obj.y = actor_new_position
-            mapOfTiles[actor_x_position][actor_map_new_position] = obj
+            mapOfTiles[actor_x_position][actor_map_new_position] = actor
             mapOfTiles[actor_x_position][actor_y_position] = new_tile
         end
+        return
     end
 end
 
 function foodNewPosition()
-    repeat
-        local food_x = math.random(0, 5) * tileWidth
-        local food_y = math.random(0, 5) * tileHeight
-        local food_x_map = food_x / tileWidth + 1
-        local food_y_map = food_y / tileHeight + 1
+    print("fui chamado", newFood)
+    
+    while newFood do
+        print("estou no loop")
+        local food_x_map = math.random(1, 5)
+        local food_y_map = math.random(1, 5)
+        local food_x = (food_x_map - 1) * tileWidth
+        local food_y = (food_y_map - 1) * tileHeight
 
         print("novo x", food_x)
         print("novo y", food_y)
         print("food_x_map ", food_x_map)
         print("food_y_map ", food_y_map)
 
-        if mapOfTiles[food_x_map][food_y_map].type ~= "food" then
+        if food_x ~= actor.x or food_y ~= actor.y then
             food.x = food_x
             food.y = food_y
-            mapOfTiles[food_x / tileWidth + 1][food_y / tileHeight + 1] = food
-            break
+            mapOfTiles[food_x_map][food_y_map] = food
+            newFood = false
         end
 
-    until true
+    end
 end
