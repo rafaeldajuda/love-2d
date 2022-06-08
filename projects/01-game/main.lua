d6fb3a803cd246be192810bc7203ce1fb78bbb49
Rafael <rafaeldajuda@gmail.com>
2022-06-08 00:00:44 -0300

    add game over

--- projects/01-game/main.lua
+++ projects/01-game/main.lua
@@ -29,22 +29,24 @@ end
 function love.update(dt)
 
     time = time + dt
-    print("time\t", time)
     if time >= 1 then
         moveActor(actor.direction)
         if newFood then
             foodNewPosition()
         end
+
+        print("time\t", time)
+        print("actor x:\t",actor.x)
+        print("actor y:\t",actor.y)
+        print("actor direction:",actor.direction)
+        print("food x:\t", food.x)
+        print("food y:\t", food.y)
+        print("random", math.random(0, 5))
+        print("tileWidth", tileWidth)
+        print("/-------------------------/")
     end
     
-    print("actor x:\t",actor.x)
-    print("actor y:\t",actor.y)
-    print("actor direction:",actor.direction)
-    print("food x:\t", food.x)
-    print("food y:\t", food.y)
-    print("random", math.random(0, 5))
-    print("tileWidth", tileWidth)
-    print("/-------------------------/")
+    
    
 end
 
@@ -54,17 +56,17 @@ function love.draw()
     for i, line in ipairs(mapOfTiles) do
         for j, tile in ipairs(line) do
             if tile.type == "tile" then
-                print("tile", i, j)
+                -- print("tile", i, j)
                 love.graphics.setColor(1, 1, 1)
                 love.graphics.rectangle(tile.mode, tile.x, tile.y, tile.width, tile.height) 
             elseif tile.type == "actor" then
                 -- draw actor
-                print("actor", i, j)
+                -- print("actor", i, j)
                 love.graphics.setColor(1, 1, 1)
                 love.graphics.rectangle(tile.mode, tile.x, tile.y, tile.width, tile.height) 
             else
-                 -- draw food
-                print("food", i, j)
+                -- draw food
+                -- print("food", i, j)
                 love.graphics.setColor(1, 0, 0)
                 love.graphics.rectangle(tile.mode, tile.x, tile.y, tile.width, tile.height) 
             end   
@@ -106,11 +108,13 @@ function newActorPosition(axis, direction)
         if actor_new_position < windowWidth and actor_new_position >= 0 then
             actor.x = actor_new_position
             if mapOfTiles[actor_map_new_position][actor_y_position] == food then
-                print("come x")
                 newFood = true
             end
             mapOfTiles[actor_map_new_position][actor_y_position] = actor
             mapOfTiles[actor_x_position][actor_y_position] = new_tile
+        else 
+            print("game over")
+            love.load()
         end
         return
     elseif axis == "y" then
@@ -119,30 +123,24 @@ function newActorPosition(axis, direction)
         if actor_new_position < windowWidth and actor_new_position >= 0  then
             actor.y = actor_new_position
             if mapOfTiles[actor_x_position][actor_map_new_position] == food then
-                print("come y")
                 newFood = true
             end
             mapOfTiles[actor_x_position][actor_map_new_position] = actor
             mapOfTiles[actor_x_position][actor_y_position] = new_tile
+        else
+            print("game over")
+            love.load()
         end
         return
     end
 end
 
 function foodNewPosition()
-    print("fui chamado", newFood)
-    
     while newFood do
-        print("estou no loop")
         local food_x_map = math.random(1, 5)
         local food_y_map = math.random(1, 5)
         local food_x = (food_x_map - 1) * tileWidth
         local food_y = (food_y_map - 1) * tileHeight
-
-        print("novo x", food_x)
-        print("novo y", food_y)
-        print("food_x_map ", food_x_map)
-        print("food_y_map ", food_y_map)
 
         if food_x ~= actor.x or food_y ~= actor.y then
             food.x = food_x
